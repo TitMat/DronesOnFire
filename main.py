cc5922dbd895fb27028770c675d5cd76929d7458
titou <titouan_mathieu@hotmail.com>
2021-05-02 21:20:38 +0200

    Déplacement illustré parfaitement grâce à tortle. Ajout d'un petit carré rouge qui s'immule le feu.

--- main.py
+++ main.py
@@ -56,32 +56,77 @@ def parcourirZone(drone, zone):
     print("Parcours de la zone jusqu'aux coordonnées : " + str(zone.latitude2) + " x " + str(zone.longitude2))
     feuTrouve = False
     tur.pendown()
-    while drone.latitude < zone.latitude2 and feuTrouve is False:
-        while drone.longitude < zone.longitude2 and drone.longitude >= zone.longitude and feuTrouve is False:
+    while drone.latitude < zone.latitude2: #and feuTrouve is False:
+        while drone.longitude < zone.longitude2 and drone.longitude >= zone.longitude: #and feuTrouve is False:
             if getTemperature(drone) > 50:
                 print("*** Feu suspecté à la position : " + str(drone.latitude) + " x " + str(drone.longitude))
-                feuTrouve = True
-                tur.penup()
-            drone.longitude = drone.longitude + (-1)**(drone.deplacement)
-            tur.forward((-1)**(drone.deplacement)*10)
-        if (-1)**(drone.deplacement) == (1):
+                #feuTrouve = True
+                #tur.penup()
+                showfire()
+            drone.longitude = drone.longitude + (-1) ** (drone.deplacement)
+            tur.forward((-1) ** (drone.deplacement) * 10)
+        if (-1) ** (drone.deplacement) == (1):
             tur.right(90)
-            tur.forward(10)
             drone.latitude = drone.latitude + 1
+            if drone.latitude < zone.latitude2:
+                tur.forward(10)
+
             tur.left(90)
         else:
             tur.right(90)
-            tur.forward(10)
             drone.latitude = drone.latitude + 1
+            if drone.latitude < zone.latitude2:
+                tur.forward(10)
+
             tur.left(90)
         drone.deplacement = drone.deplacement + 1
         drone.longitude = drone.longitude + (-1)**(drone.deplacement)
     tur.penup()
-    if (-1)**(drone.deplacement):
-        tur.left(180)
+
+    if drone.id == 1 or drone.id == nombreDrones:
+        drone.latitude = zone.latitude2
+        if drone.id == 1:
+            drone.longitude = zone.longitude
+        else:
+            drone.longitude = zone.longitude2
+        if (-1) ** (drone.deplacement) == -1:
+            if drone.id == 1:
+                tur.right(180)
+                tur.forward(largeurSecteur * 10)
+                tur.left(180)
+        if (-1) ** (drone.deplacement) == 1:
+            if drone.id == nombreDrones:
+                tur.forward(largeurSecteur * 10)
+        tur.left(90)
+        tur.pendown()
+        tur.pencolor("black")
+        for i in range(zone.latitude2-1):
+            drone.longitude = drone.longitude + (-1)
+            tur.forward(10)
+        tur.penup()
+        tur.right(90)
+
+
+def showfire():
+    print(tur.fillcolor())
+    tur.fillcolor("red")
+    tur.begin_fill()
+    tur.right(90)
+    tur.forward(3)
+    tur.left(90)
+    for i in range(4):
+        tur.forward(6)
+        tur.left(90)
+    tur.end_fill()
+    tur.pencolor("black")
+    tur.left(90)
+    tur.forward(3)
+    tur.right(90)
+
 
 
 # Instanciation et envoi drones
 for i in range(1, nombreDrones+1):
     parcourirZone(Drone(i, "Drone"+str(i), 0, 0, 0), Zone(i, "Secteur"+str(i), z1.latitude, z1.longitude + largeurSecteur * (i-1), z1.latitude2, z1.longitude + largeurSecteur * i))
 
+turtle.done()
